067ef997b0eb337a7b76ce169c37a51986055c33
craigwann <craigwann@me.com>
2018-08-03 13:32:23 -0700

    pass age in seconds test

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -1,14 +1,18 @@
 import { Galactic } from './../src/galactic.js';
 
 describe('Galactic', function() {
-//   var reusableDate;
-//
-//   beforeEach(function() {
-//     reusableDate = new Galactic('June 14,2000');
-//   });
+  var reusableDate;
 
-
-  it('should test a persons age', function() {
-    // expect(reusableDate.getAge()).toEqual(18);
+  beforeEach(function() {
+    reusableDate = new Galactic('June 14,2000');
   });
+
+  it('should test a persons age in years', function() {
+    expect(reusableDate.getAge()).toEqual(18);
+  });
+
+  it('should test a persons age in seconds', function() {
+    expect(reusableDate.getAgeSeconds()).toEqual(572313600);
+  });
+
 });
--- src/galactic.js
+++ src/galactic.js
@@ -1,12 +1,16 @@
 export class Galactic{
   constructor(dateString) {
     this.dateString = dateString;
-    this.today = new Date();
+    this.today = new Date(2018, 7, 3);
     this.birthDate = new Date(this.dateString);
     this.age = this.today.getFullYear() - this.birthDate.getFullYear();
   }
   getAge() {
     return this.age;
+  }
+  getAgeSeconds() {
+    let seconds = parseInt((this.today - this.birthDate)/1000)
+    return seconds;
   }
 
 
